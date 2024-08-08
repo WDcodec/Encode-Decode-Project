c904cd585f3d57a385c1f3c2641eaf29829f23c8
user1 <618tamar@gmail.com>
2024-08-08 11:59:40 +0300

    define field element header functions

--- Encoder-Decoder-System/Encoder-Decoder-System/Field_Element.cpp
+++ Encoder-Decoder-System/Encoder-Decoder-System/Field_Element.cpp
@@ -1,1 +1,10 @@
 #include "Field_Element.h"
+
+inline wd_codec::galois::Field_Element operator + (const wd_codec::galois::Field_Element& a, const wd_codec::galois::Field_Element& b);
+inline wd_codec::galois::Field_Element operator - (const wd_codec::galois::Field_Element& a, const wd_codec::galois::Field_Element& b);
+inline wd_codec::galois::Field_Element operator * (const wd_codec::galois::Field_Element& a, const wd_codec::galois::Field_Element& b);
+inline wd_codec::galois::Field_Element operator * (const wd_codec::galois::Field_Element& a, const wd_codec::galois::Field_Element& b);
+inline wd_codec::galois::Field_Element operator * (const wd_codec::galois::Field_Element& a, const wd_codec::galois::Field_Element& b);
+inline wd_codec::galois::Field_Element operator / (const wd_codec::galois::Field_Element& a, const wd_codec::galois::Field_Element& b);
+inline wd_codec::galois::Field_Element operator ^ (const wd_codec::galois::Field_Element& a, const int& b);
+
--- Encoder-Decoder-System/Encoder-Decoder-System/Field_Element.h
+++ Encoder-Decoder-System/Encoder-Decoder-System/Field_Element.h
@@ -10,7 +10,36 @@ namespace wd_codec {
 		public: 
 			Field_Element(const Field& field, const field_symbol& value = 0);
 			Field_Element(const Field_Element& element);
-			~Field_Element() {};
+			Field_Element(const Field& gfield);
+			~Field_Element() {}; 
+
+			inline Field_Element& operator = (const Field_Element& gfe);
+			inline Field_Element& operator = (const field_symbol& v);
+			inline Field_Element& operator += (const Field_Element& gfe);
+			inline Field_Element& operator += (const field_symbol& v);
+			inline Field_Element& operator -= (const Field_Element& gfe);
+			inline Field_Element& operator -= (const field_symbol& v);
+			inline Field_Element& operator *= (const Field_Element& gfe);
+			inline Field_Element& operator *= (const field_symbol& v);
+			inline Field_Element& operator /= (const Field_Element& gfe);
+			inline Field_Element& operator /= (const field_symbol& v);
+			inline Field_Element& operator ^= (const int& n);
+			inline bool operator == (const Field_Element& gfe) const;
+			inline bool operator == (const field_symbol& v) const;
+			inline bool operator != (const Field_Element& gfe) const;
+			inline bool operator != (const field_symbol& v) const;
+			inline bool operator < (const Field_Element& gfe);
+			inline bool operator < (const field_symbol& v);
+			inline bool operator > (const Field_Element& gfe);
+			inline bool operator > (const field_symbol& v);
+			inline field_symbol index() const;
+			inline field_symbol poly() const;
+			inline field_symbol& poly();
+			inline const Field& galois_field() const;
+			inline field_symbol inverse() const;
+			inline void normalize();
+			friend std::ostream& operator << (std::ostream& os, const Field_Element& gfe);
+
 		private:
 			const Field& field_;
 			//the value of the coafficient
