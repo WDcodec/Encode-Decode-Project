d5f3045ff7b6f70d33320d839fdeef6c7bb5ecae
user1 <618tamar@gmail.com>
2024-08-11 11:17:14 +0300

    define polynomial header function

--- Encoder-Decoder-System/Encoder-Decoder-System/Polynomial.cpp
+++ Encoder-Decoder-System/Encoder-Decoder-System/Polynomial.cpp
@@ -1,1 +1,31 @@
 #include "Polynomial.h"
+namespace wd_codec
+{
+
+    namespace galois
+    {
+        Polynomial operator + (const Polynomial& a, const Polynomial& b);
+        Polynomial operator + (const Polynomial& a, const Field_Element& b);
+        Polynomial operator + (const Polynomial& a, const Polynomial& b);
+        Polynomial operator + (const Polynomial& a, const field_symbol& b);
+        Polynomial operator + (const field_symbol& a, const Polynomial& b);
+        Polynomial operator - (const Polynomial& a, const Polynomial& b);
+        Polynomial operator - (const Polynomial& a, const Field_Element& b);
+        Polynomial operator - (const Field_Element& a, const Polynomial& b);
+        Polynomial operator - (const Polynomial& a, const field_symbol& b);
+        Polynomial operator - (const field_symbol& a, const Polynomial& b);
+        Polynomial operator * (const Polynomial& a, const Polynomial& b);
+        Polynomial operator * (const Field_Element& a, const Polynomial& b);
+        Polynomial operator * (const Polynomial& a, const Field_Element& b);
+        Polynomial operator / (const Polynomial& a, const Polynomial& b);
+        Polynomial operator / (const Polynomial& a, const Field_Element& b);
+        Polynomial operator % (const Polynomial& a, const Polynomial& b);
+        Polynomial operator % (const Polynomial& a, const unsigned int& power);
+        Polynomial operator ^ (const Polynomial& a, const int& n);
+        Polynomial operator <<(const Polynomial& a, const unsigned int& n);
+        Polynomial operator >>(const Polynomial& a, const unsigned int& n);
+        Polynomial         gcd(const Polynomial& a, const Polynomial& b);
+        inline Polynomial generate_X(const Field& gfield);
+
+    }
+}
--- Encoder-Decoder-System/Encoder-Decoder-System/Polynomial.h
+++ Encoder-Decoder-System/Encoder-Decoder-System/Polynomial.h
@@ -5,7 +5,7 @@
 
 #include "Field.h"
 #include "Field_Element.h"
-
+#include "Polynomial.h"
 
 namespace wd_codec
 {
@@ -15,6 +15,59 @@ namespace wd_codec
 
 		class Polynomial
 		{
+            Polynomial(const Field& gfield);
+            Polynomial(const Field& gfield, const unsigned int& degree);
+            Polynomial(const Field& gfield, const unsigned int& degree, const field_element element[]);
+            Polynomial(const Polynomial& polynomial);
+            Polynomial(const Field_Element& gfe);
+            ~Polynomial() {}
+
+            bool valid() const;
+            int deg() const;
+            const Field& galois_field() const;
+            void set_degree(const unsigned int& x);
+            void simplify();
+
+            Polynomial& operator  =  (const Polynomial& polynomial);
+            Polynomial& operator  =  (const Field_Element& element);
+            Polynomial& operator +=  (const Polynomial& element);
+            Polynomial& operator +=  (const Field_Element& element);
+            Polynomial& operator -=  (const Polynomial& element);
+            Polynomial& operator -=  (const Field_Element& element);
+            Polynomial& operator *=  (const Polynomial& polynomial);
+            Polynomial& operator *=  (const Field_Element& element);
+            Polynomial& operator /=  (const Polynomial& divisor);
+            Polynomial& operator /=  (const Field_Element& element);
+            Polynomial& operator %=  (const Polynomial& divisor);
+            Polynomial& operator %=  (const unsigned int& power);
+            Polynomial& operator ^=  (const unsigned int& n);
+            Polynomial& operator <<= (const unsigned int& n);
+            Polynomial& operator >>= (const unsigned int& n);
+
+            Field_Element& operator[] (const std::size_t& term);
+            Field_Element     operator() (const Field_Element& value);
+            Field_Element     operator() (field_symbol                 value);
+
+            const Field_Element& operator[](const std::size_t& term) const;
+            const Field_Element  operator()(const Field_Element& value) const;
+            const Field_Element  operator()(field_symbol         value) const;
+
+            bool operator==(const Polynomial& polynomial) const;
+            bool operator!=(const Polynomial& polynomial) const;
+
+            bool monic() const;
+
+            Polynomial derivative() const;
+
+            friend std::ostream& operator << (std::ostream& os, const field_polynomial& polynomial);
+
+        private:
+
+            typedef std::vector<Field_Element>::iterator       poly_iter;
+            typedef std::vector<Field_Element>::const_iterator const_poly_iter;
+
+            void simplify(Polynomial& polynomial) const;
+
 			Field& field_;
 			std::vector<Field_Element> poly_;
 
