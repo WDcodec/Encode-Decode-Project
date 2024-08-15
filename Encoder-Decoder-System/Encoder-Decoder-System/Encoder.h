ddc898f4f5b0f140bcc55965fb45bc3f1e740705
Efrat Eisenbach <efratizen@gmail.com>
2024-08-15 13:46:10 +0300

    implement posit_data_in_codeword function

--- Encoder-Decoder-System/Encoder-Decoder-System/Encoder.h
+++ Encoder-Decoder-System/Encoder-Decoder-System/Encoder.h
@@ -28,9 +28,20 @@ namespace wd_codec
 			{}
 			
 			bool encode(block_type& rsblock) const;
+		private:
 			const bool encoder_valid;
 			const galois::Field& field_;
 			const galois::Polynomial generator_;
+
+			//compute the P(x)*x^(n-k) for making place to the redundancy 
+			inline const galois::Polynomial& posit_data_in_codeword(const block_type& rsblock) {
+				galois::Polynomial data(field_,code_length);
+				for (std::size_t i = data_length - 1; i >= fec_length; i--) {
+					data[i] = rsblock[data_length - 1 - i];
+				}
+				return data;
+			}
+
 		};
 	}
 }
