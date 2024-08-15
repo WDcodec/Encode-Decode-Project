9c370ed8b8c6835f58fd9ce24951ce3fab94d706
user1 <618tamar@gmail.com>
2024-08-15 12:39:56 +0300

    implement encoder ctor & dtor

--- Encoder-Decoder-System/Encoder-Decoder-System/Encoder.h
+++ Encoder-Decoder-System/Encoder-Decoder-System/Encoder.h
@@ -6,24 +6,27 @@
 #include "Field.h"
 #include "Polynomial.h"
 #include "Field_Element.h"
-
+#include "Block.h"
 namespace wd_codec
 {
 
-	namespace galois
+	namespace reed_solomon
 	{
 		template <std::size_t code_length, std::size_t fec_length, std::size_t data_length = code_length - fec_length>
 		class Encoder
 		{
-			encoder(const galois::field& gfield, const galois::field_polynomial& generator)
-				: encoder_valid_(code_length == gfield.size()),
+			public:
+			typedef Block<code_length, fec_length> block_type;
+
+			Encoder(const galois::Field& gfield, const galois::Polynomial& generator)
+				: encoder_valid(code_length == gfield.size()),
 				field_(gfield),
 				generator_(generator)
 			{}
 
-			~encoder()
+			~Encoder()
 			{}
-		
+			
 			bool encode(block_type& rsblock) const;
 			const bool encoder_valid;
 			const galois::Field& field_;
