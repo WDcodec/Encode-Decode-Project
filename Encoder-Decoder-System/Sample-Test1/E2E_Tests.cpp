c2d547e383ff8a8d56e991d6b1a627cad9275594
Efrat Eisenbach <efratizen@gmail.com>
2024-09-10 10:54:29 +0300

    seperate the tests to e2e and units and add test_utility

--- Encoder-Decoder-System/Sample-Test1/E2E_Tests.cpp
+++ Encoder-Decoder-System/Sample-Test1/E2E_Tests.cpp
@@ -4,22 +4,8 @@
 ////  EXPECT_EQ(1, 1);
 ////  EXPECT_TRUE(true);
 ////}
-#include <gtest/gtest.h>
-#include "setup_utilities.h"
-// Helper function to create an empty file
-void createEmptyFile(const std::string& file_name) {
-    std::ofstream file(file_name, std::ios::out | std::ios::binary);
-    // The file is created empty
-}
-//
-//// Setup and teardown functions for logging
-void setupTestEnvironment() {
-    wd_codec::Logger::init();
-}
 
-void teardownTestEnvironment() {
-    wd_codec::Logger::close();
-}
+
 
 ///Test case for handling an empty file
 TEST(EncoderDecoderTests, HandleEmptyFile) {
